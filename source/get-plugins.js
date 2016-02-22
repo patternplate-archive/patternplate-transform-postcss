afa7c4f4ae0931d195a990690cf459751ddbbb04
Mario Nebl <hello@herebecode.com>
2016-02-22 14:16:01 +0100

    fix: make plugin loading and sorting actually work

--- source/get-plugins.js
+++ source/get-plugins.js
@@ -1,6 +1,19 @@
+import {resolve} from 'try-require';
+
+function resolveable(name) {
+	if (!resolve(name)) {
+		throw new Error(`postcss plugin "${name}" is configured but could not be resolved. Is "${name}" installed?`);
+	}
+	return name;
+}
+
 export default configuration =>
 	Object.entries(configuration.plugins || {})
-		.filter((a, b) => b[1].order - a[1].order)
+		.sort((a, b) => a[1].order - b[1].order)
+		.filter(entry => {
+			const [name] = entry;
+			return resolveable(name);
+		})
 		.reduce((registry, entry) => {
 			const [name, options] = entry;
 			const plugin = require(name);
